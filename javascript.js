5013994564acda15dae296b711327c901f8d939f
Giorgi-Tabatadze <giorgi.taba@icloud.com>
2022-04-23 19:13:26 +0400

    Add RGB and darkening on each hover
    To add some colors to game each however generates random rgb box,
    on each however color gets 10% darker until becoming completely black.
    Minor changes to ui.

--- javascript.js
+++ javascript.js
@@ -2,8 +2,17 @@
 
 const container = document.querySelector("#container");
 
+function randomRGB (times) {
+  const randomBetween = (min, max) => min + Math.floor(Math.random() * (max - min + 1));
+  const r = randomBetween(0, 255);
+  const g = randomBetween(0, 255);
+  const b = randomBetween(0, 255);
+  return `rgb(${r-r*times/10},${g-g*times/10},${b-b*times/10})`;
+}
+
 function paintBox (e) {
-  this.classList.add("painted");
+  this.style.backgroundColor= `${randomRGB(this.dataset.times)}`
+  this.dataset.times++;
 };
 
 function removeAllChildNodes (parent) {
@@ -24,6 +33,7 @@ function appendDiv (id, gridNumber) {
   div.style.height = `${600/gridNumber}px`
   div.classList.toggle("box");
   div.setAttribute('id', `${id}`);        
+  div.setAttribute("data-times", 0);
   div.addEventListener("mouseover", paintBox)
   container.appendChild(div);
 };
@@ -34,6 +44,9 @@ function newGrid () {
   drawGrid(slider.value);
 }
 
+function changeSliderText (e) {
+  sliderText.textContent = `Number of boxes: ${slider.value}`;
+}
 
 
 const slider = document.querySelector("#myRange");
@@ -44,8 +57,5 @@ const sliderText = document.querySelector("#slidertext")
 sliderText.textContent = `Number of boxes: ${slider.value}`;
 slider.addEventListener("input", changeSliderText)
 
-function changeSliderText (e) {
-  sliderText.textContent = `Number of boxes: ${slider.value}`;
-}
 
-drawGrid (16);
+drawGrid (slider.value);
