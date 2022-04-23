99ca0b3d838fa8ef84bb3478fd8c0633993ddb50
Giorgi-Tabatadze <giorgi.taba@icloud.com>
2022-04-23 20:12:36 +0400

    Paint when mouse is down, make code more readable
    Boxes will get colored only when mouse button is down. This provides
    Possibility for users to draw more complex images.
    Rearanged code and wrote comments to make code more readable.

--- javascript.js
+++ javascript.js
@@ -1,32 +1,19 @@
-/*create grid which will divide div equally among 16 divs*/
-
-const container = document.querySelector("#container");
-
-function randomRGB (times) {
-  const randomBetween = (min, max) => min + Math.floor(Math.random() * (max - min + 1));
-  const r = randomBetween(0, 255);
-  const g = randomBetween(0, 255);
-  const b = randomBetween(0, 255);
-  return `rgb(${r-r*times/10},${g-g*times/10},${b-b*times/10})`;
+//Delete existing grid and draw new grid using current slider value
+function newGrid () {
+  const coloredBoxes = Array.from(document.querySelectorAll(".painted"));
+  coloredBoxes.forEach(coloredBox => {coloredBox.classList.remove("painted")});
+  drawGrid(slider.value);
 }
-
-function paintBox (e) {
-  this.style.backgroundColor= `${randomRGB(this.dataset.times)}`
-  this.dataset.times++;
-};
-
-function removeAllChildNodes (parent) {
-  while (parent.firstChild) {
-    parent.removeChild(parent.firstChild)
-  }
-};
-
 function drawGrid (gridNumber){
   removeAllChildNodes(container)
   for (let i = 0; i < gridNumber*gridNumber; i++) {
     appendDiv(i, gridNumber);
 }};
-
+function removeAllChildNodes (parent) {
+  while (parent.firstChild) {
+    parent.removeChild(parent.firstChild)
+  }
+};
 function appendDiv (id, gridNumber) {
   const div = document.createElement("div");
   div.style.width = `${600/gridNumber}px`
@@ -38,24 +25,53 @@ function appendDiv (id, gridNumber) {
   container.appendChild(div);
 };
 
-function newGrid () {
-  const coloredBoxes = Array.from(document.querySelectorAll(".painted"));
-  coloredBoxes.forEach(coloredBox => {coloredBox.classList.remove("painted")});
-  drawGrid(slider.value);
+//is mouse down?
+function mouseDown () {
+  mouseIsDown = true;
 }
 
+function mouseUp () {
+  mouseIsDown = false;
+}
+//If mouse is down Generate random RGB color to paint the box,10% darker each time
+function randomRGB (times) {
+  const randomBetween = (min, max) => min + Math.floor(Math.random() * (max - min + 1));
+  const r = randomBetween(0, 255);
+  const g = randomBetween(0, 255);
+  const b = randomBetween(0, 255);
+  return `rgb(${r-r*times/10},${g-g*times/10},${b-b*times/10})`;
+}
+function paintBox (e) {
+  if (mouseIsDown) 
+  {
+    this.style.backgroundColor= `${randomRGB(this.dataset.times)}`
+    this.dataset.times++;
+  };
+};
+//Change slider value displayed on the page
 function changeSliderText (e) {
   sliderText.textContent = `Number of boxes: ${slider.value}`;
 }
 
 
+const container = document.querySelector("#container");
+
+//Add event listener to check if mouse is down.
+const body = document.querySelector("body")
+body.addEventListener("mousedown", mouseDown)
+body.addEventListener("mouseup", mouseUp)
+let mouseIsDown = false;
+
+//If mouse is pressed initiate new grid
 const slider = document.querySelector("#myRange");
 const newButton = document.querySelector("#newgrid");
 newButton.addEventListener("click", newGrid);
-
+//Keep track of slider value
 const sliderText = document.querySelector("#slidertext")
 sliderText.textContent = `Number of boxes: ${slider.value}`;
 slider.addEventListener("input", changeSliderText)
 
+newGrid ();
 
-drawGrid (slider.value);
+/* Code by: Giorgi Tabatadze 
+Written during learning through The Odin Project*/
